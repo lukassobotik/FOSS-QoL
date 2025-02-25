ad7e58ecd2e1a8a65df01292466ceb253d7ed3c5
lukassobotik <lukassobotik@users.noreply.github.com>
2025-02-25 18:03:14 +0100

    CleanShare improvements (#4)

--- app/src/main/java/dev/lukassobotik/fossqol/CleanShareActivity.kt
+++ app/src/main/java/dev/lukassobotik/fossqol/CleanShareActivity.kt
@@ -1,5 +1,6 @@
 package dev.lukassobotik.fossqol
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -9,56 +10,83 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import dev.lukassobotik.fossqol.ui.theme.FOSSQoLTheme
 import kotlinx.coroutines.launch
 
 class CleanShareActivity : ComponentActivity() {
+    private var selectedUri by mutableStateOf<Uri?>(null)
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var sharedUri: Uri? = null
-
         if (Intent.ACTION_SEND == intent.action && intent.type != null) {
-            sharedUri = intent.getParcelableExtra(Intent.EXTRA_STREAM)
+            selectedUri = intent.getParcelableExtra(Intent.EXTRA_STREAM)
         }
 
         enableEdgeToEdge()
         setContent {
             val tintColor = if (isSystemInDarkTheme()) Color.White else Color.Black
             FOSSQoLTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(),
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
                     topBar = {
                         topAppBar(
                             context = this@CleanShareActivity,
                             label = "Clean Share",
                             showBackButton = true,
-                            tintColor = tintColor)
-                    }) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        MetadataRemoverScreen(
-                            sourceUri = sharedUri)
+                            tintColor = tintColor
+                        )
+                    }
+                ) { innerPadding ->
+                    Column (modifier = Modifier.padding(innerPadding), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                        MetadataRemoverScreen(sourceUri = selectedUri)
                     }
                 }
             }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Codes.REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK) {
+            data?.data?.let { uri ->
+                selectedUri = uri
+                // Persist permission to access the selected file
+                contentResolver.takePersistableUriPermission(
+                    uri,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                )
+            }
+        }
+    }
+}
+object Codes {
+    internal const val REQUEST_CODE_SELECT_IMAGE = 1001
 }
 
 @Composable
 fun MetadataRemoverScreen(sourceUri: Uri?) {
-    if (sourceUri == null) {return}
     val context = LocalContext.current
     var cleanUri by remember { mutableStateOf<Uri?>(null) }
     val scope = rememberCoroutineScope()
@@ -67,13 +95,27 @@ fun MetadataRemoverScreen(sourceUri: Uri?) {
         modifier = Modifier.padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        // Button to select an image
         Button(onClick = {
-            // Launch a coroutine to process the image
-            scope.launch {
-                cleanUri = removeMetadata(context, sourceUri)
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "image/*"
             }
+            (context as? Activity)?.startActivityForResult(intent, Codes.REQUEST_CODE_SELECT_IMAGE)
         }) {
-            Text("Remove Metadata")
+            Text("Select Image")
+        }
+
+        // Show the "Remove Metadata" button only if an image is selected
+        sourceUri?.let {
+            Button(onClick = {
+                // Launch a coroutine to process the image
+                scope.launch {
+                    cleanUri = removeMetadata(context, it)
+                }
+            }) {
+                Text("Remove Metadata")
+            }
         }
 
         cleanUri?.let { uri ->
@@ -92,3 +134,4 @@ fun MetadataRemoverScreen(sourceUri: Uri?) {
         }
     }
 }
+
